dfea6f21b71e3971c78a123dc7483a25c7720252
Saagar Jha <saagar@saagarjha.com>
2016-10-11 21:15:52 -0700

    Remove ignored files

--- OpenAdBlock/Pods/Target Support Files/Pods-OpenAdBlock/Pods-OpenAdBlock-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_OpenAdBlock : NSObject
-@end
-@implementation PodsDummy_Pods_OpenAdBlock
-@end
--- OpenAdBlock/Pods/Target Support Files/Pods-OpenAdBlockUITests/Pods-OpenAdBlockUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_OpenAdBlockUITests : NSObject
-@end
-@implementation PodsDummy_Pods_OpenAdBlockUITests
-@end
